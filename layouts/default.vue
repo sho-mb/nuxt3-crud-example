8c87d7f8208795b59b9da504572501616fff1db0
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-08-12 14:05:18 +0700

    chore: add eslint formatter

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-rose-400">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    1dnndnn
+    <div>
+      dd
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
